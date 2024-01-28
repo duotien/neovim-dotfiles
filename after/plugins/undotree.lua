736b4932d9a815d18d0b3388d2371679fd1bb9dd
duotien <notduotien@gmail.com>
2024-01-29 00:36:44 +0700

    Toggle and focus on undotree

--- after/plugins/undotree.lua
+++ after/plugins/undotree.lua
@@ -1,1 +1,6 @@
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+--vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+-- Define a function that toggles and focuses on undotree
+
+-- Map the leader key to the custom function
+vim.keymap.set('n', '<leader>u', function() vim.cmd.UndotreeToggle() vim.cmd.UndotreeFocus() end)
+
